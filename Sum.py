75b5be1bf897f85891a0416cb9bc695ff2efd813
Rahul7171 <69434755+Rahul7171@users.noreply.github.com>
2020-11-26 20:19:13 +0530

    Add files via upload

---
+++ Sum.py
@@ -0,0 +1,13 @@
+print("Program for Implementing Arithemtic Opearations")
+print(55+45)
+print(85*22)
+print(23%6)
+print(1006-500)
+print("Finally Implemented the first program of python")
+********OUTPUT*********
+Program for Implementing Arithemtic Opearations
+100
+1870
+5
+506
+Finally Implemented the first program of python
